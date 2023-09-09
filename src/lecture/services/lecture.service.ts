9641344e0b84872c705d7c64fcee8b95c0254d3f
LJH <passby158@gmail.com>
2023-09-09 16:08:47 +0900

    Feat(#5): lecture create 구현

--- src/lecture/services/lecture.service.ts
+++ src/lecture/services/lecture.service.ts
@@ -5,14 +5,18 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class LectureService {
   constructor(private readonly prismaService: PrismaService) {}
+
   async createLecture(
     lecture: CreateLectureDto,
     danceLecturerId: number,
   ): Promise<CreateLectureDto> {
-    lecture.reservationDeadline = new Date(lecture.reservationDeadline);
-    const newLecture = await this.prismaService.lecture.create({
-      data: { danceLecturerId, ...lecture },
+    const reservationDeadline = new Date(lecture.reservationDeadline);
+    return await this.prismaService.lecture.create({
+      data: {
+        danceLecturerId,
+        ...lecture,
+        reservationDeadline,
+      },
     });
-    return newLecture;
   }
 }
