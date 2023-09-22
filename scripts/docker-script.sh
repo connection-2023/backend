ef389eeb1d3f7108364ec2e8ff81ed4eb16e6750
kimsoo0119 <illppang@naver.com>
2023-09-22 18:25:15 +0900

    Feat(#2): 도커실행을 위간 기본 파일 추가

---
+++ scripts/docker-script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+DOCKER_IMAGE_NAME=server-img
+
+DOCKER_CONTAINER_NAME=server-container
+
+docker rm -f $(docker ps -qa)
+
+docker build -t ${DOCKER_IMAGE_NAME} . # <--- 프로젝트 루트 경로에 대한 상대경로
+
+docker run -d -p 3001:3000 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
