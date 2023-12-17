12e2ada360f37658e24c24c352a014bc944e486e
LJH <passby158@gmail.com>
2023-12-17 21:58:15 +0900

    Fix(#215): 강사 내 리뷰 조회시 정렬 에러 orderBy 배열로 삽입

--- src/lecture/services/lecture-review.service.ts
+++ src/lecture/services/lecture-review.service.ts
@@ -282,22 +282,27 @@ export class LectureReviewService {
       where['lectureId'] = lectureId;
     }
 
-    const order = { id: 'desc' };
+    const order = [];
+    order.push({ id: 'desc' });
 
     if (orderBy === '최신순') {
-      order['reservation'] = {
-        lectureSchedule: {
-          startDateTime: 'desc',
+      order.push({
+        reservation: {
+          lectureSchedule: {
+            startDateTime: 'desc',
+          },
         },
-      };
+      });
     } else if (orderBy === '좋아요순') {
-      order['likedLectureReview'] = {
-        _count: 'desc',
-      };
+      order.push({
+        likedLectureReview: {
+          _count: 'desc',
+        },
+      });
     } else if (orderBy === '평점 높은순') {
-      order['stars'] = 'desc';
+      order.push({ stars: 'desc' });
     } else if (orderBy === '평점 낮은순') {
-      order['stars'] = 'asc';
+      order.push({ stars: 'asc' });
     }
 
     const isPagination = currentPage && targetPage;
