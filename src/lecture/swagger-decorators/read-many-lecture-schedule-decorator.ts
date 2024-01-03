66a9739e2a884e476c02adbf62828bacd0a1caec
LJH <passby158@gmail.com>
2024-01-03 11:25:58 +0900

    Feat(#228): 스웨거 리스펀스 수정

--- src/lecture/swagger-decorators/read-many-lecture-schedule-decorator.ts
+++ src/lecture/swagger-decorators/read-many-lecture-schedule-decorator.ts
@@ -1,5 +1,9 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
 import { SwaggerApiResponse } from '@src/common/swagger/swagger-response';
 
 export function ApiReadManyLectureSchedule() {
@@ -7,40 +11,51 @@ export function ApiReadManyLectureSchedule() {
     ApiOperation({
       summary: '강의 스케쥴 조회',
     }),
-    ApiCreatedResponse(
-      SwaggerApiResponse.success('강의 리뷰 조회완료', {
+    ApiOkResponse(
+      SwaggerApiResponse.success('강의 스케쥴 조회완료', {
         statusCode: 200,
         data: {
           schedule: [
             {
-              id: 69,
-              lectureId: 25,
+              id: 319,
+              lectureId: 93,
               startDateTime: '2023-10-03T11:00:00.000Z',
               endDateTime: '2023-10-03T13:00:00.000Z',
               numberOfParticipants: 0,
-              team: null,
             },
             {
-              id: 70,
-              lectureId: 25,
+              id: 320,
+              lectureId: 93,
               startDateTime: '2023-10-03T11:00:00.000Z',
               endDateTime: '2023-10-03T13:00:00.000Z',
               numberOfParticipants: 0,
-              team: null,
             },
             {
-              id: 71,
-              lectureId: 25,
+              id: 321,
+              lectureId: 93,
               startDateTime: '2023-10-03T11:00:00.000Z',
               endDateTime: '2023-10-03T13:00:00.000Z',
               numberOfParticipants: 0,
-              team: null,
             },
           ],
           holidayArr: [
             '2023-10-03T11:00:00.000Z',
             '2023-10-03T11:00:00.000Z',
             '2023-10-03T11:00:00.000Z',
+          ],
+          daySchedule: [
+            {
+              id: 1,
+              lectureId: 93,
+              day: ['월', '수', '금'],
+              dateTime: ['13:00:00', '14:00:00'],
+            },
+            {
+              id: 2,
+              lectureId: 93,
+              day: ['수'],
+              dateTime: ['13:00:00'],
+            },
           ],
         },
       }),
