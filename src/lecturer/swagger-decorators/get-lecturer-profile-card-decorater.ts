31e71601f34d542ce41169ffd04523911227a55e
kimsoo0119 <illppang@naver.com>
2024-01-24 00:12:28 +0900

    Refactor(#212): 강사의 간략한 프로필 조회 로직 변경 완료

--- src/lecturer/swagger-decorators/get-lecturer-profile-card-decorater.ts
+++ src/lecturer/swagger-decorators/get-lecturer-profile-card-decorater.ts
@@ -1,25 +1,19 @@
 import { ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
-import { applyDecorators } from '@nestjs/common';
+import { HttpStatus, applyDecorators } from '@nestjs/common';
 import { SwaggerApiResponse } from '@src/common/swagger/swagger-response';
+import { DetailResponseDto } from '@src/common/swagger/dtos/detail-response-dto';
+import { LecturerBasicProfileDto } from '../dtos/lecturer-basic-profile.dto';
 
 export function ApiGetLecturerBasicProfile() {
   return applyDecorators(
     ApiOperation({
       summary: '강사 프로필 간략한 정보 조회',
-      description: '토큰 필요',
     }),
     ApiBearerAuth(),
-    ApiOkResponse(
-      SwaggerApiResponse.success('강사 정보 반환', {
-        statusCode: 200,
-        data: {
-          lecturerBasicProfile: {
-            id: 3,
-            profileCardImageUrl: 'url',
-            nickname: '올리버쌤',
-          },
-        },
-      }),
+    DetailResponseDto.swaggerBuilder(
+      HttpStatus.OK,
+      'lecturerBasicProfile',
+      LecturerBasicProfileDto,
     ),
   );
 }
