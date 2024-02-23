41043870a39d56493c999058c039c723f6421291
LJH <passby158@gmail.com>
2024-02-24 00:54:05 +0900

    Feat(#373): 신청 강의 결제 정보 type데코 적용

--- src/lecture/dtos/enroll-schedule-detail-price.dto.ts
+++ src/lecture/dtos/enroll-schedule-detail-price.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { PaymentMethodDto } from '@src/payments/dtos/payment-method.dto';
-import { Exclude, Expose } from 'class-transformer';
+import { Exclude, Expose, Type } from 'class-transformer';
 
 @Exclude()
 export class EnrollScheduleDetailPriceDto {
@@ -14,6 +14,7 @@ export class EnrollScheduleDetailPriceDto {
 
   @Expose()
   @ApiProperty({ description: '결제 수단', type: PaymentMethodDto })
+  @Type(() => PaymentMethodDto)
   paymentMethod: PaymentMethodDto;
 
   constructor(payment: Partial<EnrollScheduleDetailPriceDto>) {
