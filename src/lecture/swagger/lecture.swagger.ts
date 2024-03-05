7756de5c54ebad599974d97a43c76e39b00986f6
LJH <passby158@gmail.com>
2024-03-05 15:46:28 +0900

    Feat(#409): 공통 스웨거 추가

---
+++ src/lecture/swagger/lecture.swagger.ts
@@ -0,0 +1,108 @@
+import { ApiOperator } from '@src/common/types/type';
+import { LectureController } from '../controllers/lecture.controller';
+import { HttpStatus, applyDecorators } from '@nestjs/common';
+import { OperationObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+import { ApiOperation } from '@nestjs/swagger';
+import { DetailResponseDto } from '@src/common/swagger/dtos/detail-response-dto';
+import { GeneralResponseDto } from '@src/common/swagger/dtos/general-response.dto';
+import { CombinedScheduleDto } from '../dtos/combined-schedule.dto';
+
+export const ApiLecture: ApiOperator<keyof LectureController> = {
+  GetLectureSchedule: (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator => {
+    return applyDecorators(
+      ApiOperation(apiOperationOptions),
+      GeneralResponseDto.swaggerBuilder(
+        HttpStatus.OK,
+        'lectureSchedule',
+        CombinedScheduleDto,
+      ),
+    );
+  },
+  CreateLecture: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  ReadLecturePreview: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  ReadLectureDetail: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  ReadManyLecture: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  DeleteLecture: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  UpdateLecture: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  ReadLectureScheduleLearnerList: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  ReadLectureReservation: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  GetLectureLearnerList: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  ReadManyEnrollLectureWithUserId: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  GetEnrollLectureList: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  ReadManyLectureSchedules: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  ReadManyLectureDailySchedules: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+  GetEnrollScheduleDetail: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    throw new Error('Function not implemented.');
+  },
+};
