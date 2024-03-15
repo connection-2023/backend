cf900be2d2e681334c74ca67094a00d30bc03b03
LJH <passby158@gmail.com>
2024-03-15 19:14:48 +0900

    Refactor(#433): 강의 리뷰 조회 dto수정

--- src/lecture/dtos/combined-lecture-review-with-count.dto.ts
+++ src/lecture/dtos/combined-lecture-review-with-count.dto.ts
@@ -1,17 +1,3 @@
-import { LectureReviewDto } from '@src/common/dtos/lecture-review.dto';
-import { ILectureReview } from '../interface/lecture.interface';
 import { LecturerReviewResultDto } from './read-lecturer-review.dto';
 
-export class CombinedLectureReviewWithCountDto extends LecturerReviewResultDto {
-  // constructor(lectureReviews: {
-  //   reviews?: ILectureReview[];
-  //   reviewCount?: number;
-  // }) {
-  //   super(lectureReviews);
-  //   this.reviews = lectureReviews.reviews
-  //     ? lectureReviews.reviews.map((review) => new LectureReviewDto(review))
-  //     : [];
-  //   this.reviewCount = lectureReviews.reviewCount;
-  //   Object.assign(this);
-  // }
-}
+export class CombinedLectureReviewWithCountDto extends LecturerReviewResultDto {}
