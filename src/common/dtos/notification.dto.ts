cce4d326cca10386dadc78aed07b3156aec6a48e
LJH <passby158@gmail.com>
2024-04-22 13:20:50 +0900

    Feat(#397): 알림 reservationId 반환 값 추가

--- src/common/dtos/notification.dto.ts
+++ src/common/dtos/notification.dto.ts
@@ -31,6 +31,11 @@ export class NotificationDto {
   lectureId: number;
 
   @Expose()
+  @ApiProperty({ description: '강의 id', type: Number })
+  @Type(() => Number)
+  reservationId: number;
+
+  @Expose()
   @ApiProperty({ description: '강사 id', type: Number })
   lecturerId: number;
 
