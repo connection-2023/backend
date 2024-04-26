233500729ca47d47908abc833e17d4a4cd5919c6
LJH <passby158@gmail.com>
2024-04-26 19:04:12 +0900

    Refactor(#490): docker compose 명령어 수정 및 레거시 도커 명령어 삭제

--- scripts/docker-script.sh
+++ scripts/docker-script.sh
@@ -2,15 +2,10 @@
 
 DOCKER_IMAGE_NAME=server-img
 
-# DOCKER_CONTAINER_NAME=server-container
-
 docker system prune -a -f
 
 docker rm -f $(docker ps -qa)
 
 docker build -t ${DOCKER_IMAGE_NAME} . # <--- 프로젝트 루트 경로에 대한 상대경로
 
-ls
-
-# docker run -d -p 3001:3000 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
-docker-compose up -d
+docker compose up -d
