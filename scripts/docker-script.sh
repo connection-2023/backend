26078399f2a4497e8447b469e649b9710fdc8bf4
LJH <passby158@gmail.com>
2024-04-26 19:18:54 +0900

    Refactor(#490): docker compose 명령어 파일 위치 지정

--- scripts/docker-script.sh
+++ scripts/docker-script.sh
@@ -8,4 +8,4 @@ docker rm -f $(docker ps -qa)
 
 docker build -t ${DOCKER_IMAGE_NAME} . # <--- 프로젝트 루트 경로에 대한 상대경로
 
-docker compose up -d
+docker compose -f /home/ubuntu/backend/Docker-compose.yml up -d
