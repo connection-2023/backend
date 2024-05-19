028fd6b568d11cb3d7cf086b188d9b417a84ced5
kimsoo0119 <illppang@naver.com>
2024-05-19 16:05:43 +0900

    Refactor(#424): 결제 내역 조회 서비스 코드 리팩터링 완료

--- src/payments/services/user-payments.service.ts
+++ src/payments/services/user-payments.service.ts
@@ -4,9 +4,7 @@ import {
   NotFoundException,
   OnModuleInit,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { PaymentsRepository } from '@src/payments/repository/payments.repository';
-import { PrismaService } from '@src/prisma/prisma.service';
 import { GetUserPaymentsHistoryDto } from '@src/payments/dtos/get-user-payments-history.dto';
 import { CreateBankAccountDto } from '@src/payments/dtos/create-bank-account.dto';
 import { UserBankAccountDto } from '@src/payments/dtos/user-bank-account.dto';
@@ -15,35 +13,20 @@ import { PaymentHistoryTypes } from '../constants/enum';
 import { DetailPaymentInfoDto } from '../dtos/response/detail-payment.dto';
 import { plainToInstance } from 'class-transformer';
 import { VirtualAccountDepositDetailsDto } from '../dtos/response/virtual-account-deposit-details.dto';
+import { generatePaginationParams } from '@src/common/utils/generate-pagination-params';
 
 @Injectable()
-export class UserPaymentsService implements OnModuleInit {
-  private readonly logger = new Logger(UserPaymentsService.name);
-
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly paymentsRepository: PaymentsRepository,
-    private readonly prismaService: PrismaService,
-  ) {}
-
-  onModuleInit() {
-    this.logger.log('PaymentsService Init');
-  }
+export class UserPaymentsService {
+  constructor(private readonly paymentsRepository: PaymentsRepository) {}
 
   async getUserPaymentsHistory(
-    {
-      take,
-      currentPage,
-      targetPage,
-      firstItemId,
-      lastItemId,
-      paymentHistoryType,
-    }: GetUserPaymentsHistoryDto,
+    dto: GetUserPaymentsHistoryDto,
     userId: number,
   ): Promise<{
     totalItemCount: Number;
     userPaymentsHistory?: DetailPaymentInfoDto[];
   }> {
+    const { paymentHistoryType, ...paginationOptions } = dto;
     const paymentTypeId =
       paymentHistoryType === PaymentHistoryTypes.전체
         ? undefined
@@ -58,14 +41,8 @@ export class UserPaymentsService implements OnModuleInit {
       return { totalItemCount };
     }
 
-    const paginationParams: IPaginationParams = this.getPaginationParams(
-      currentPage,
-      targetPage,
-      firstItemId,
-      lastItemId,
-      take,
-    );
-
+    const paginationParams: IPaginationParams =
+      generatePaginationParams(paginationOptions);
     const userPaymentsHistory =
       await this.paymentsRepository.getUserPaymentHistory(
         userId,
@@ -113,33 +90,6 @@ export class UserPaymentsService implements OnModuleInit {
       await this.paymentsRepository.getUserRecentBankAccount(userId);
 
     return plainToInstance(UserBankAccountDto, selectedBankAccount);
-  }
-
-  private getPaginationParams(
-    currentPage: number,
-    targetPage: number,
-    firstItemId: number,
-    lastItemId: number,
-    take: number,
-  ): IPaginationParams {
-    let cursor;
-    let skip;
-    let updatedTake = take;
-
-    const isPagination = currentPage && targetPage;
-    const isInfiniteScroll = lastItemId && take;
-
-    if (isPagination) {
-      const pageDiff = currentPage - targetPage;
-      cursor = { id: pageDiff <= -1 ? lastItemId : firstItemId };
-      skip = Math.abs(pageDiff) === 1 ? 1 : (Math.abs(pageDiff) - 1) * take + 1;
-      updatedTake = pageDiff >= 1 ? -take : take;
-    } else if (isInfiniteScroll) {
-      cursor = { id: lastItemId };
-      skip = 1;
-    }
-
-    return { cursor, skip, take: updatedTake };
   }
 
   async getUserReceipt(
